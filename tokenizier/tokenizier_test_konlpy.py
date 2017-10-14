e6778d7cdde65ce440c8490326066b765c113229
HongGiwon <gch02518@skku.edu>
2017-10-14 21:16:52 +0900

    refine

--- tokenizier/tokenizier_test_konlpy.py
+++ tokenizier/tokenizier_test_konlpy.py
@@ -1,5 +1,12 @@
+import codecs
 from konlpy.tag import Kkma, Twitter
 
 tmp = Twitter().pos("프로젝트에 참여해서 관련 문서에 기여의 손길을 보내주세요!")
 
-print (tmp)
+
+input_dump = codecs.open("namu_001.txt", 'r', "utf-8")
+
+dump_line = input_dump.readline()
+print(dump_line)
+
+input_dump.close()
