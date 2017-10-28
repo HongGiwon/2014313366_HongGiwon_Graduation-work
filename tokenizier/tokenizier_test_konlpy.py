373b5218a5162c01919e6ee52e20856fec3965e3
HongGiwon <gch02518@skku.edu>
2017-10-29 05:16:11 +0900

    전처리

--- tokenizier/tokenizier_test_konlpy.py
+++ tokenizier/tokenizier_test_konlpy.py
@@ -7,45 +7,37 @@ import pickle
 from konlpy.tag import Twitter
 import re
 
-corpus = []
+for i in range(1,21) :
 
-count = 0
+	corpus = []
+	count = 0
 
-for i in range(7,8) :
-
-	print (i)
-	input_dump = codecs.open("pnamu_020.txt", 'r', "utf-8")
-	tokenized_output = open("toknamu_00" + str(i) + ".txt", 'wb')
+	input_dump = codecs.open("pnamu_" + str(i) + ".txt", 'r', "utf-8")
 
 	while (True) :
 	
 		if (count % 100 == 0) : print (count)
+		
 		count = count + 1
-
-		print (count)
 		dump_line = input_dump.readline()
 
 		if not dump_line : break
 
-		print (count)
 		if (len(dump_line) < 5) : 
 			continue
-		print (count)
+
 		token_tmp = Twitter().pos(dump_line)
-		print (count)
 		tokenized_sentence = ["{}/{}".format(word, tag) for word, tag in token_tmp if (tag != "Punctuation" and tag != "Foreign")]
 
 		if (len(tokenized_sentence) == 0) :
 			continue
 
-		print (count)
+
 		corpus.append(tokenized_sentence)
 
-
-	#바이너리 형식으로 토크나이징 된 결과물을 저장함.
-	pickle.dump(corpus,tokenized_output)
-	print (count)
-
 	input_dump.close()
+	#바이너리 형식으로 토크나이징 된 결과물을 저장함.
+	tokenized_output = open("toknamu_" + str(i) + ".txt", 'wb')
+	pickle.dump(corpus,tokenized_output)
 	tokenized_output.close()
 
