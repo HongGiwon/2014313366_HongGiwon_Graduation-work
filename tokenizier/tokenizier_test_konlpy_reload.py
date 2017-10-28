efff951ecf3d608517cca2e5b995fa0a6d6a864d
HongGiwon <gch02518@skku.edu>
2017-10-29 05:17:47 +0900

    모델 완성
    Former-commit-id: 848e08e3ba4474077da7514ad4df3606b176451a

--- tokenizier/tokenizier_test_konlpy_reload.py
+++ tokenizier/tokenizier_test_konlpy_reload.py
@@ -2,44 +2,56 @@
 
 import pickle
 import gensim
+import os
+import multiprocessing
 
-tokenized_output = open("toknamu_020.txt", 'rb')
+class iter_multxt2sen(object):
 
-#corpus = []
+    def __init__(self, top_dir):
+        self.file_list = []
+        for root, dirs, files in os.walk(top_dir) :
+        	for fname in filter(lambda fname: fname.endswith('.txt'), files) :
+        		self.file_list.append(os.path.join(root, fname))
 
-#토크나이징 된 단어들을 읽어서 corpus에 저장
-#try : 
-#	while (True) :
-#
-#		tmplist = pickle.load(tokenized_output)
-#		corpus.append(tmplist)
-#
-#except EOFError :
-#	print ("EOF")
-#	tokenized_output.close()
+    def __iter__(self):
 
-corpus = pickle.load(tokenized_output)
-tokenized_output.close()
+    	for filename in self.file_list :
+    		print (filename)
+    		txt_file = open(filename,'rb')
+    		for sentence in pickle.load(txt_file) :
+    			yield sentence
+
+corpus = iter_multxt2sen(os.getcwd() + "/result")
 
 #word2vec 모델의 설정
 config = {
-    'min_count': 1,
+    'min_count': 10,
     'size': 300,
     'sg': 1,
     'batch_words': 10000,
-    'iter': 10
+    'iter': 10,
+    'workers': multiprocessing.cpu_count()
 }
 
 #모델 생성
 model = gensim.models.Word2Vec(**config)
 
 #모델 사전 생성 및 학습
-model.build_vocab(corpus)
+print ("사전 생성")
+model.build_vocab(corpus, update=False)
+print ("학습 시작")
 model.train(corpus, total_examples=model.corpus_count, epochs=model.iter)
 
+print ("저장 시작")
 #모델 저장
 model.save('model')
-#model = gensim.models.Word2Vec.load('model')
+
+#모델 단어 개수 출력
+print(len(model.wv.vocab))
+
+#online 학습
+#model.build_vocab(corpus, update=True)
+#model.train(corpus, total_examples=model.corpus_count, epochs=model.iter)
 
 #두 단어의 유사성 예시
 print(model.similarity('카메라/Noun', '유튜브/Noun'))
