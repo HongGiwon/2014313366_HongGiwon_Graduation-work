0e4f47f4ca1225af12007ae17c88c6a415e94076
HongGiwon <gch02518@skku.edu>
2017-10-29 05:18:08 +0900

    몇가지 예제
    Former-commit-id: 7fcd73a2eb6bbd67f0779f6d0209d8d0fef455c4

--- tokenizier/tokenizier_test_konlpy_reload_load.py
+++ tokenizier/tokenizier_test_konlpy_reload_load.py
@@ -5,6 +5,13 @@ import gensim
 
 model = gensim.models.Word2Vec.load('model')
 
-print(model.most_similar(positive=["서울/Noun"], negative=["일본/Noun"], topn=5))
+print(model.most_similar(positive=["서울/Noun", "일본/Noun"], negative=["한국/Noun"], topn=5))
+#model.wv.most_similar_cosmul
 
+print(model.most_similar(positive=["지구/Noun"], negative=["생명/Noun"], topn=5))
 
+print(model.most_similar(positive=["사람/Noun"], negative=["지능/Noun"], topn=5))
+
+print(model.most_similar(positive=["개새끼/Noun", "씨발/Noun"], topn=5))
+
+#print(model.most_similar_cosmul(positive=["개새끼/Noun"], topn=10))
